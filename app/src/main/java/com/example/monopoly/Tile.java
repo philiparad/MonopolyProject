0edc8de5fd37385a95b25931c40b6ff6111e2bf1
Philip Arad <philip.arad@gmail.com>
2025-08-08 16:13:03 +0300

    Define Monopoly board tiles

--- app/src/main/java/com/example/monopoly/Tile.java
+++ app/src/main/java/com/example/monopoly/Tile.java
@@ -4,14 +4,18 @@ public class Tile {
     public String name;
     public TileType type;
     public int price;
+    public int rent;
+    public String colorGroup;
     public boolean isOwned;
     public int ownerId;
     public int houseCount;
 
-    public Tile(String name, TileType type, int price) {
+    public Tile(String name, TileType type, int price, int rent, String colorGroup) {
         this.name = name;
         this.type = type;
         this.price = price;
+        this.rent = rent;
+        this.colorGroup = colorGroup;
         this.isOwned = false;
         this.ownerId = -1;
         this.houseCount = 0;
